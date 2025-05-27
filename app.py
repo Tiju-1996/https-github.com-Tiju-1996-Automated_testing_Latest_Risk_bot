eefd4513c2d8555fb7cabcca7fe0fb6e99dda392
Tiju-1996 <tiju.varghese@beinex.com>
2025-05-27 19:40:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -281,7 +281,7 @@ else:
     if 'risk_msgs' not in st.session_state:
         st.session_state.risk_msgs = []
     llm_audit = ChatNVIDIA(
-        model="google/gemma-3-27b-it",
+        model="tiiuae/falcon3-7b-instruct",
         api_key= NVIDIA_API_KEY,
         temperature=0, num_ctx=50000
     )
